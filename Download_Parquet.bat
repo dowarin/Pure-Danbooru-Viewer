d9aaa55a89900d8ad988baa23e161fa24ab383f4
Bondarchuk <irinabondaruchuku@gmail.com>
2025-03-28 21:00:19 +0900

    Update Download_Parquet.bat

--- Download_Parquet.bat
+++ Download_Parquet.bat
@@ -2,12 +2,12 @@
 setlocal enabledelayedexpansion
 
 if not exist "parquet" ( mkdir "parquet" )
-curl -oparquet\Dan_post.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Dan_post.parquet
-curl -oparquet\Dan_rels.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Dan_rels.parquet
-curl -oparquet\Dan_tags.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Dan_tags.parquet
-curl -oparquet\Gel_post.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Gel_post.parquet
-curl -oparquet\Gel_rels.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Gel_rels.parquet
-curl -oparquet\Gel_tags.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Gel_tags.parquet
-curl -oparquet\tarIndex_alphachannel.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/tarIndex_alphachannel.parquet
-curl -oparquet\tarIndex_duplicate.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/tarIndex_duplicate.parquet
-curl -oparquet\tarIndex_image.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/tarIndex_image.parquet
+curl -o parquet\Dan_post.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Dan_post.parquet
+curl -o parquet\Dan_rels.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Dan_rels.parquet
+curl -o parquet\Dan_tags.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Dan_tags.parquet
+curl -o parquet\Gel_post.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Gel_post.parquet
+curl -o parquet\Gel_rels.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Gel_rels.parquet
+curl -o parquet\Gel_tags.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/Gel_tags.parquet
+curl -o parquet\tarIndex_alphachannel.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/tarIndex_alphachannel.parquet
+curl -o parquet\tarIndex_duplicate.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/tarIndex_duplicate.parquet
+curl -o parquet\tarIndex_image.parquet -L https://huggingface.co/datasets/dowarin/Pure-Danbooru-Viewer-Parquet/resolve/main/tarIndex_image.parquet
